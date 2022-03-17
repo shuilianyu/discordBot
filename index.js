6103027c98f49acef48a3f9c9587b596f5c30ce4
杨福波 <yangfubo@yangfubodeMacBook-Pro.local>
2022-03-17 17:44:53 +0800

    public

--- index.js
+++ index.js
@@ -1,21 +1,24 @@
 require('dotenv').config(); //initialize dotenv
 const Discord = require('discord.js'); //import discord.js
 
-const client = new Discord.Client({ intents: [Discord.Intents.FLAGS.GUILDS]}); //create new client
+const client = new Discord.Client({
+    intents: [Discord.Intents.FLAGS.GUILDS]
+}); //create new client
 // const client = new Discord.Client({ intents: [Discord.Intents.FLAGS.GUILDS], fetchAllMembers: true});
 
 
 
 client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
+    console.log(`Logged in as ${client.user.tag}!`);
 });
 
-client.on('message', msg => {
-    if (msg.content === 'ping') {
-      msg.reply('Pong!');
-    }
-  });
 
+client.on('message', msg => {
+    console.log('content: ', msg.content);
+    if (msg.content === 'ping') {
+        msg.reply('Pong!');
+    }
+});
 
 // console.log('process.env.CLIENT_TOKEN: ', process.env.CLIENT_TOKEN);
 //make sure this line is the last line
