de548a84be85857af1de65c67a0953ca5b30de96
杨福波 <yangfubo@yangfubodeMacBook-Pro.local>
2022-03-17 17:32:18 +0800

    public

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 require('dotenv').config(); //initialize dotenv
 const Discord = require('discord.js'); //import discord.js
 
-const client = new Discord.Client({ intents: [Discord.Intents.FLAGS.GUILDS], proxy:  'http://127.0.0.1:7890'}); //create new client
+const client = new Discord.Client({ intents: [Discord.Intents.FLAGS.GUILDS]}); //create new client
 // const client = new Discord.Client({ intents: [Discord.Intents.FLAGS.GUILDS], fetchAllMembers: true});
 
 
@@ -9,6 +9,12 @@ const client = new Discord.Client({ intents: [Discord.Intents.FLAGS.GUILDS], pro
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
+
+client.on('message', msg => {
+    if (msg.content === 'ping') {
+      msg.reply('Pong!');
+    }
+  });
 
 
 // console.log('process.env.CLIENT_TOKEN: ', process.env.CLIENT_TOKEN);
