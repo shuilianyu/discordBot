569ebfa2675cde80c5703697a045d3bce5875c10
杨福波 <yangfubo@yangfubodeMacBook-Pro.local>
2022-03-18 15:53:19 +0800

    public

--- index.js
+++ index.js
@@ -9,5 +9,11 @@ client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
 });
 
+client.on('message', msg => {
+    if (msg.content === 'ping') {
+        msg.reply('Pong!');
+    }
+});
+
 //make sure this line is the last line
 client.login(process.env.CLIENT_TOKEN); //login bot using token
