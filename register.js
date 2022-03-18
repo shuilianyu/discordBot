b18d5e97a8e0f1dd1ae2dd79f8a11a62cf59ece6
杨福波 <yangfubo@yangfubodeMacBook-Pro.local>
2022-03-18 16:19:03 +0800

    public

---
+++ register.js
@@ -0,0 +1,30 @@
+const { REST } = require('@discordjs/rest');
+const { Routes } = require('discord-api-types/v9');
+
+const commands = [{
+  name: 'ping',
+  description: 'Replies with Pong!'
+}]; 
+
+const CLIENT_ID = '954272961991434270';
+const GUILD_ID = '954268721973694494';
+
+
+
+
+const rest = new REST({ version: '9' }).setToken('token');
+
+(async () => {
+  try {
+    console.log('Started refreshing application (/) commands.');
+
+    await rest.put(
+      Routes.applicationGuildCommands(CLIENT_ID, GUILD_ID),
+      { body: commands },
+    );
+
+    console.log('Successfully reloaded application (/) commands.');
+  } catch (error) {
+    console.error(error);
+  }
+})();
